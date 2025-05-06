9ea5fa1e1f0b88c0d4237bbc27328e7496ef7356
Sameer Kashyap <41501069+MistakenPirate@users.noreply.github.com>
2025-05-06 11:24:06 +0530

    CHORE: simple variables in rust

--- rust_project/src/main.rs
+++ rust_project/src/main.rs
@@ -1,10 +1,36 @@
 fn main() {
-    let x: i32 = -5;
-    let y: u32 = 1000;
-    let z: f32 = 1000.001;
-
+    // let x: i32 = -5;
+    // let y: u32 = 1000;
+    // let z: f32 = 1000.001;
+    // print!("x = {}, y = {}, z = {}\n", x, y, z);
     //if we dont give a type rust by default infers any integer as i32 and float as f64
 
-    print!("x = {}, y = {}, z = {}\n", x, y, z);
-    println!("Welcome to my first rust code!")
+    // let mut x: i8 = 10000;
+    // for i in 0..1000 {
+    //     x = x + 100;
+    // }
+    // print!("x = {}\n", x);
+    //by default all variables in rust are mutable, unless mut is defined explicitly
+
+    // bools
+    // let is_male : bool = true;
+    // let is_above_18 : bool = true;
+    // if is_male{
+    //     println!("You are male")
+    // } else {
+    //     println!("You are not a male")
+    // }
+    // if is_male && is_above_18{
+    //     println!("You are an adult")
+    // }
+
+    let greeting : String = String::from("Hello World");
+    println!("{}", greeting);
+    let char1: Option<char> = greeting.chars().nth(1);
+    match char1{
+        Some(c) => println!("{}", c),
+        None => println!("No character at index 1000"),
+    }
+
+
 }
